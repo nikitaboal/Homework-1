65792116bab57e3ca9b3b15456bf756980001864
Nikita Boldaev <nikbold-02@yandex.ru>
2020-10-17 20:06:48 +0300

    Add Main class for MyFloat

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyFloat a = new MyFloat(2543748,3);
+        MyFloat b = new MyFloat(4327128,4);
+        a.dif(b);
+        a.sum(b);
+    }
+}
